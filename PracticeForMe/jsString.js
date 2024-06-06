d9b26d46c1842d3d547ee4544bcb7e19c8acf595
Mahadeb <maitysusanta824@gmail.com>
2024-06-06 22:51:49 +0530

    Create jsString.js

---
+++ PracticeForMe/jsString.js
@@ -0,0 +1,56 @@
+// for of loop  are only use to print array value & string value 
+// let  name = "mahadebmaity"
+// let i=1;
+// for(let value of name){
+//     console.log( " value= ",value );
+//     i++;
+// }
+// console.log( " length is =",i);
+
+// for in loops are only use to print object keys value 
+// let  student = {
+// name: "mahadeb maity",
+// age: 21,
+// address: "manikara",
+// studiesfrom: "PBCA "
+// };
+// for(let keys in student){
+//     console.log( " student keys=",keys ," value of keys= ", student[keys]);
+// }
+
+ //string all methods 
+ // at first length method of string  using template literals 
+//  let name ="mahadeb maity"
+//     let length=name.length;
+//  console.log(`length of ${name}  is  ${length}`);
+
+//  convert string upper case to lower case & lower case to upper case 
+
+// let name= "mahadeb"
+//  let store = name.toUpperCase()
+// console.log(" this is a toUpperCase method:",store);
+// let store1 = name.toLowerCase()
+// console.log( "  this is a toLowerCase method:",store1);
+// // slice method applied
+//  let slice= name.slice(0,7)
+//  console.log(" this is a slice method : ",slice);
+
+// let  title = " Maity"
+// let concatination= name.concat(title) 
+// let charat = name.charAt(0)
+// // console.log(charat);
+// console.log(`${concatination} ${charat}`);
+
+/* question is that : create a game where you start 
+any random game number . ask the user keep geussing 
+the number  until the user geussing correct value then 
+print congratulation you win this  game 
+*/
+// gaming question for me 
+// let GameNumber = 20
+//  let userNumber= prompt("geuss  the game number:");
+// while( userNumber != GameNumber){
+//     userNumber= prompt(" sorry this is wrong, geuss again")
+// }
+// console.log( " congratulation you win this geuss");
+
