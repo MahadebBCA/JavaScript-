b6993e8074f2675f53ef78f10a98dc0b123e278f
Mahadeb <maitysusanta824@gmail.com>
2024-06-07 23:07:38 +0530

    Create array.js

---
+++ PracticeForMe/array.js
@@ -0,0 +1,45 @@
+// // practice Array  in javascript
+// let  name = [" mahadeb","jayanta"," debotosh", " surya kanta"];
+// console.log(name);
+// let index =0;
+// //  print array value  using  for of loop & template literals 
+// for(let value of name){
+// console.log(`the  index of ${index} value  is ${value}`);
+// index++;
+// }
+// //calculate length of fa array
+// console.log(` the length of the array is ${name.length}`);
+// let  Number =[54,65,34,78,89];
+// let sum = 0;
+//  for(let count of Number){
+//     sum = sum + count;
+//  }
+//  let length=Number.length;
+//  let average= sum/length;
+//  console.log(" the length of the array is ",length);
+//  console.log(" the total of all numbers is ",sum);
+//  console.log(" the average of all marks is", average);
+
+ // apply array methods in javascript 
+ // push()
+
+//  let name =[ "Mahadeb maity"," debatosh  mama","janta santa  ", "biswajit dalal"];
+ // update value of  a name array
+//   name.push("atanu dan");
+//   console.log(name);
+//   // remove value of a name array 
+// name.pop();
+// console.log(name);
+// name.toString();
+// console.log(name);
+// console.log(typeof name);
+// name.unshift(" madhurima");
+// console.log(name);
+// name.shift();
+// console.log(name);
+//  let store = name.slice(2,4);
+// console.log(store);
+// let store1 = name.splice(1,3, "ms dhoni" , "yubaraj shing");
+// console.log( " this is a removed items ", store1);
+// console.log( " this is a new array " ,name);
+
